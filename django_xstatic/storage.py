7198947957e3ef277faf5ccc67b588622194a3ab
Christophe Siraut <d@tobald.eu.org>
2021-12-09 14:58:13 +0100

    python3: use importlib

--- django_xstatic/storage.py
+++ django_xstatic/storage.py
@@ -1,6 +1,9 @@
 from django.core.exceptions import ImproperlyConfigured
 from django.core.files.storage import FileSystemStorage
-from django.utils.importlib import import_module
+try:
+    from importlib import import_module
+except ImportError:
+    from django.utils.importlib import import_module
 
 
 class XStaticStorage(FileSystemStorage):
